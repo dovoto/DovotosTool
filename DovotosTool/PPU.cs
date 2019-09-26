086875fcc8ab79f651ee9e25f4465cea808fb042
dovoto <jason@drunkencoders.com>
2019-09-26 12:48:28 -0700

    Update PPU.cs
    Added some fake register writing

--- DovotosTool/PPU.cs
+++ DovotosTool/PPU.cs
@@ -1,26 +1,52 @@
 public class PPU
 {
-    private static int reg2000_control;
-    private static int reg2001_mask;
-    private static int reg2002_status;
-    private static int reg2003_oamAddress;
-    private static int reg2004_oamData;
-    private static int reg2005_scroll;
-    private static int reg2006_vramAddress;
-    private static int reg2007_vramData;
+    public static byte reg2000_control;
+    public static byte reg2001_mask;
+    public static byte reg2002_status;
+    public static byte reg2003_oamAddress;
+    public static byte reg2004_oamData;
+    public static byte reg2005_scroll;
+    public static byte reg2006_vramAddress;
+    public static byte reg2007_vramData;
     
-    private static byte[] PPU_RAM = new byte[0x800];
+    public static byte[] PPU_RAM = new byte[0x800];
     
     public static Color[] FrameBuffer = new Color[256*256];
     
-    public static byte[] OAM = new byte[40*4];
+    public static byte[] OAM = new byte[64*4];
     
     public static byte Read(int address)
     {
     }
     
-    public static Write(int address, int byte)
+    public static Write(int address, byte d)
     {
+        case(address & 0x7)
+        {
+            0:
+                reg2000_control = d;
+                break;
+            1:
+                reg2001_mask = d;
+                break;
+            2:
+                reg2002_control = d;
+                break;
+            3:
+                reg2003_oamAddress = d;
+                break;
+            4:
+                reg2004_oamData = d;
+                break;
+            5:
+                reg2005_scroll = d;
+                break;
+            6:
+                reg2007_vramData = d;
+                break;
+            7:
+            default: break;
+        }
     }
     
     
